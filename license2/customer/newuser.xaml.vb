bf939c4ba0f3e2f2d4c3dd696412627b56ec3605
shirhaan <adhude@live.com>
2016-07-13 17:52:00 +0500

    c

--- license2/customer/newuser.xaml.vb
+++ license2/customer/newuser.xaml.vb
@@ -1,4 +1,6 @@
 ﻿Public Class newuser
+    Dim island_id As Integer
+
     Private Sub btn_close_Click(sender As Object, e As RoutedEventArgs) Handles btn_close.Click
         Me.Close()
 
@@ -19,11 +21,69 @@
         Dim DatabaseDataSetAtollTableAdapter As license2.databaseDataSetTableAdapters.AtollTableAdapter = New license2.databaseDataSetTableAdapters.AtollTableAdapter()
         DatabaseDataSetAtollTableAdapter.Fill(DatabaseDataSet.Atoll)
         Dim AtollViewSource As System.Windows.Data.CollectionViewSource = CType(Me.FindResource("AtollViewSource"), System.Windows.Data.CollectionViewSource)
-        AtollViewSource.View.MoveCurrentToFirst
+        AtollViewSource.View.MoveCurrentToFirst()
+        'Load data into the table Island. You can modify this code as needed.
+        Dim DatabaseDataSetIslandTableAdapter As license2.databaseDataSetTableAdapters.IslandTableAdapter = New license2.databaseDataSetTableAdapters.IslandTableAdapter()
+        DatabaseDataSetIslandTableAdapter.Fill(DatabaseDataSet.Island)
+        Dim IslandViewSource As System.Windows.Data.CollectionViewSource = CType(Me.FindResource("IslandViewSource"), System.Windows.Data.CollectionViewSource)
+        IslandViewSource.View.MoveCurrentToFirst()
+    End Sub
+
+    Private Sub AtollComboBox_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles AtollComboBox.SelectionChanged
+
+
+    End Sub
+
+    Private Sub AtollComboBox_LostKeyboardFocus(sender As Object, e As KeyboardFocusChangedEventArgs) Handles AtollComboBox.LostKeyboardFocus
+        combo_island.Items.Clear()
+        Dim atoll_id As Integer
+        atoll_id = AtollComboBox.SelectedValue
+        Dim island As String
+        Dim atollcount As Integer
+        Dim index As Integer = 0
+
+
+
+        Dim db As New databaseDataSetTableAdapters.IslandTableAdapter
+
+        atollcount = db.GetDataBy(atoll_id).Count
+
+
+        Do
+            island = db.GetDataBy(atoll_id).Rows(index).Item(1)
+            combo_island.Items.Add(island)
+
+
+            index += 1
+        Loop Until index = atollcount
+
+    End Sub
+
+    Private Sub combo_island_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles combo_island.SelectionChanged
+
+
+
+
     End Sub
 
 
 
 
+    Function getislandid()
 
+        Dim island As String
+
+        island = combo_island.SelectedItem.ToString
+
+        Dim db As New databaseDataSetTableAdapters.IslandTableAdapter
+        island_id = db.IslandidGetDataBy(combo_island.Text).Rows(0).Item(0)
+
+        MsgBox(island_id)
+
+    End Function
+
+    Private Sub btn_submit_Click(sender As Object, e As RoutedEventArgs) Handles btn_submit.Click
+        Call getislandid()
+
+    End Sub
 End Class
