ffcd89252849f647fe257e4cd518bcaf27e19b57
ahdhude <ahdhude@gmail.com>
2016-08-08 04:55:47 -0700

    welcome form 1

--- license2/welcome/welcome.xaml.vb
+++ license2/welcome/welcome.xaml.vb
@@ -3,6 +3,12 @@
 Public Class welcome
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
 
+        Dim DatabaseDataSet As license2.databaseDataSet = CType(Me.FindResource("DatabaseDataSet"), license2.databaseDataSet)
+        'Load data into the table customer. You can modify this code as needed.
+        Dim DatabaseDataSetcustomerTableAdapter As license2.databaseDataSetTableAdapters.customerTableAdapter = New license2.databaseDataSetTableAdapters.customerTableAdapter()
+        DatabaseDataSetcustomerTableAdapter.Fill(DatabaseDataSet.customer)
+        Dim CustomerViewSource As System.Windows.Data.CollectionViewSource = CType(Me.FindResource("CustomerViewSource"), System.Windows.Data.CollectionViewSource)
+        CustomerViewSource.View.MoveCurrentToFirst()
     End Sub
 
     Private Sub PopupBox_OnOpened(sender As Object, e As RoutedEventArgs)
