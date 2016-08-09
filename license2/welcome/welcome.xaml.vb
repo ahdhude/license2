7f3232a593142bf82e59aad463099361b55682df
shirhaan <adhude@live.com>
2016-08-09 13:50:41 +0500

    customerinfo2

--- license2/welcome/welcome.xaml.vb
+++ license2/welcome/welcome.xaml.vb
@@ -9,6 +9,11 @@ Public Class welcome
         DatabaseDataSetcustomerTableAdapter.Fill(DatabaseDataSet.customer)
         Dim CustomerViewSource As System.Windows.Data.CollectionViewSource = CType(Me.FindResource("CustomerViewSource"), System.Windows.Data.CollectionViewSource)
         CustomerViewSource.View.MoveCurrentToFirst()
+        Id_cardComboBox.Text = Nothing
+        stack_customerinfo.Visibility = False
+
+
+
     End Sub
 
     Private Sub PopupBox_OnOpened(sender As Object, e As RoutedEventArgs)
@@ -42,5 +47,38 @@ Public Class welcome
     Private Sub Window_MouseMove(sender As Object, e As MouseEventArgs)
         Me.Effect = Nothing
 
+    End Sub
+
+    Sub custinfo()
+        Dim inputid As String
+
+        Dim atoll As String
+        Dim island As String
+
+
+        inputid = Id_cardComboBox.Text
+
+        Dim db As New databaseDataSetTableAdapters.customerTableAdapter
+        label_name.Content = db.GetcustInfo(inputid).Rows(0).Item(1)
+        label_address.Content = db.GetcustInfo(inputid).Rows(0).Item(3)
+        atoll = db.GetcustInfo(inputid).Rows(0).Item(8)
+        island = db.GetcustInfo(inputid).Rows(0).Item(9)
+        label_island.Content = atoll & " " & island
+        label_contact.Content = "+960 " & db.GetcustInfo(inputid).Rows(0).Item(5)
+
+
+
+
+        'Dim db As New databaseDataSetTableAdapters.customerTableAdapter
+        'idnum = db.GetData().Rows(0).Item(2)
+
+
+    End Sub
+
+
+
+
+    Private Sub Id_cardComboBox_DropDownClosed(sender As Object, e As EventArgs) Handles Id_cardComboBox.DropDownClosed
+        Call custinfo()
     End Sub
 End Class
